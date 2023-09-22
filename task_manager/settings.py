0d99ca10e48da0c07848d667fec95177699aeb20
mojnomiya <mearn4706@gmail.com>
2023-09-23 00:44:08 +0600

    Added Login/Register Template

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import Config
 
@@ -132,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import View
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'tasks/index.html'
+
+class RegisterView(TemplateView):
+    template_name = 'tasks/register.html'
+
+class LoginView(TemplateView):
+    template_name = 'tasks/login.html'
